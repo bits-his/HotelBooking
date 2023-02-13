60dee7441f157e53575366ffc356e475c07274ae
ahmad kabir <atomakabir001@gmail.com>
2023-02-13 14:32:15 +0100

    save

--- src/CustomerReg.jsx
+++ src/CustomerReg.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Card, Col, Input, Row } from 'reactstrap'
+import InputForm from './Component/InputForm'
 
 export default function CustomerReg() {
-  return (
-    <Card>
+    const [form, setForm] = useState({
+        name_of_cus: '',
+        no_of_rooms: '',
+        date_from: '',
+        date_to: '',
+        agent_name: ''
+    })
+    // const handleAdd = (e) => {
+    //     e.preventDefault()
+    //     if( 
+    //         form.name_of_cus === '' ||
+    //         form.agent_name === '' ||
+    //         form.no_of_rooms === ''
+    //      ){
+    //         alert('Input All values')
+    //      }
+    //      if (form.name_of_cus&&form.agent_name&&form.no_of_rooms) {
+    //         setForm({
+    //             name_of_cus: '',
+    //             no_of_rooms: '',
+    //             agent_name: ''
+    //         })
+    //      }
+    // }
 
-    </Card>
+    const handleChange = ({ target: { name, value } }) => {
+        setForm((p) => ({ ...p, [name]: value }));
+        console.log(form)
+    }
+
+  return (
+    <Row className='m-0 p-0'>
+        <Col md= {5} style={{margin: 'auto'}}>
+            <Card body className='mt-4' >
+                <InputForm 
+                    label='Name of Customer'
+                    value= {form.name_of_cus}
+                    onChange={handleChange}
+                    name= 'name_of_cus'
+                />
+                <InputForm 
+                    label='No. of Rooms'
+                    type= 'number'
+                    value= {form.no_of_rooms}
+                    onChange={handleChange}
+                    name= 'no_of_rooms'
+                />
+                <InputForm 
+                    label='From'
+                    type= 'date'
+                    value= {form.date_from}
+                    onChange={handleChange}
+                    name= 'date_from'
+                />
+                <InputForm 
+                    label='To'
+                    type= 'date'
+                    value= {form.date_to}
+                    onChange={handleChange}
+                    name= 'date_to'
+                />
+                <InputForm 
+                    label='Agent Name'
+                    value= {form.agent_name}
+                    onChange={handleChange}
+                    name= 'agent_name'
+                />
+                <Button style={{marginTop: 20, fontWeight: 500, fontSize: 18}}>Save</Button>
+            </Card>
+        </Col>
+    </Row>
   )
 }
