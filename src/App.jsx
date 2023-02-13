bce7c8505206f3f1062e5d0a36d85430dffc2d30
ahmad kabir <atomakabir001@gmail.com>
2023-02-13 14:43:27 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import CustomerReg from './CustomerReg'
+import HotelReg from './HotelReg'
 
 function App() {
 
   return (
-    <CustomerReg />
+    // <CustomerReg />
+    <HotelReg />
   )
 }
 
---
+++ src/HotelReg.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { Button, Card, Col, Input, Row } from 'reactstrap'
+import InputForm from './Component/InputForm'
+
+export default function HotelReg() {
+    const [form, setForm] = useState({
+        name: '',
+        address: '',
+        floor: '',
+        room_no: '',
+        price: ''
+    })
+
+    const handleChange = ({ target: { name, value } }) => {
+        setForm((p) => ({ ...p, [name]: value }));
+        console.log(form)
+    }
+
+  return (
+    <Row className='m-0 p-0'>
+        <Col md= {5} style={{margin: 'auto'}}>
+            <Card body className='mt-4' >
+                <InputForm 
+                    label='Name'
+                    value= {form.name}
+                    onChange={handleChange}
+                    name= 'name'
+                />
+                <InputForm 
+                    label='Address'
+                    value= {form.address}
+                    onChange={handleChange}
+                    name= 'address'
+                />
+                <InputForm 
+                    label='Floor'
+                    type= 'number'
+                    value= {form.floor}
+                    onChange={handleChange}
+                    name= 'floor'
+                />
+                <InputForm 
+                    label='Room No.'
+                    type= 'number'
+                    value= {form.room_no}
+                    onChange={handleChange}
+                    name= 'room_no'
+                />
+                <InputForm 
+                    label='Price'
+                    type= 'number'
+                    value= {form.price}
+                    onChange={handleChange}
+                    name= 'price'
+                />
+                <Button style={{marginTop: 20, fontWeight: 500, fontSize: 18}}>Save</Button>
+            </Card>
+        </Col>
+    </Row>
+  )
+}
