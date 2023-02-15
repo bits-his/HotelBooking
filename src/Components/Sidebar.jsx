20d3d5cf94ecb470aa8477c41ace0074bd7cf14c
ahmad kabir <atomakabir001@gmail.com>
2023-02-15 17:01:35 +0100

    save

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { MdDashboard } from 'react-icons/md'
+import { FaHotel } from 'react-icons/fa'
+import { MdOutlineBedroomParent } from 'react-icons/md'
+
 export default function Sidebar() {
   const goto = useNavigate()
     return (
-    <div className=''>
-      <h4 className='mt-2 text-white app_title'>Hotel Booking</h4>
-      <div>
-        <li onClick={()=>goto('/dashboard')}>Dashboard</li>
-        <li onClick={()=>goto('/hotel-registration')}>New Hotel</li>
-        <li onClick={()=>goto('/room-registration')}>New Room</li>
+    <div className='sidebar_div'>
+      <h4 className='mt-4 text-white app_title' style={{fontSize: '26px'}}>Hotel Booking</h4>
+      <div className= 'link' activeclassName= "active">
+        <li onClick={()=>goto('/dashboard')} className= 'link_item'><MdDashboard className='logo'/> Dashboard</li>
+        <li onClick={()=>goto('/hotel-registration')} className= 'link_item'><FaHotel className='logo'/> New Hotel</li>
+        <li onClick={()=>goto('/room-registration')} className= 'link_item'><MdOutlineBedroomParent className='logo'/> New Room</li>
       </div>
     </div>
   )
--- src/Routes/AppIndex.jsx
+++ src/Routes/AppIndex.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FaHotel } from 'react-icons/fa'
+import { MdDashboard, MdOutlineBedroomParent } from 'react-icons/md'
 import { Outlet } from 'react-router-dom'
 import { Col, Row } from 'reactstrap'
 import CustomerReg from '../Components/CustomerReg'
@@ -6,12 +9,25 @@ import Dashboard from '../Components/Dashboard'
 import Navbar from '../Components/Navbar'
 import Sidebar from '../Components/Sidebar'
 export default function AppIndex() {
+  const goto = useNavigate()
+
   return (
     <div>
         <Row className='m-0'>
           <Col md={2} className="sidebar">
             <Sidebar />
           </Col>
+          <div className='mobile_menu'>
+            <div>
+              <MdDashboard onClick={()=>goto('/dashboard')}/>
+            </div>
+            <div>
+              <FaHotel onClick={()=>goto('/hotel-registration')}/>
+            </div>
+            <div>
+              <MdOutlineBedroomParent onClick={()=>goto('/room-registration')}/>
+            </div>
+          </div>
           <Col md={10} className="">
             {/* <Navbar /> */}
             <Row>
