22be058a35202714701b52c2d7dc7a08908c2f9c
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2023-03-08 11:42:03 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { useCallback, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useLocation, useNavigate } from 'react-router-dom'
 import AppNavigation from './Routes/AppNavigation'
-
+import { init } from './redux/actions/authActions'
 function App() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const initUser = useCallback(() => {
+    dispatch(
+      init(
+        () => {
+          navigate(`${location.pathname}${location.search}`)
+        },
+        () => {
+          navigate('')
+        },
+      ),
+    )
+  }, [])
+
+  useEffect(() => {
+    initUser()
+  }, [initUser])
   return (
     <div>
       <AppNavigation />
