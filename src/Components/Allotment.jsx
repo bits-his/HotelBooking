e68d49c1f00b02e9b02d9f892d5f2fca757dad26
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2023-03-16 17:09:11 +0100

    save

--- src/Components/Allotment.jsx
+++ src/Components/Allotment.jsx
@@ -2,154 +2,176 @@ import React, { useState } from 'react'
 import { AiFillDelete } from 'react-icons/ai'
 import { Button, Card, Col, Input, Label, Row, Table } from 'reactstrap'
 import InputForm from '../CustomComponents/InputForm'
+import { _post } from '../Utils/Helper'
 import AllaotmentTable from './Table/AllaotmentTable'
 
 export default function Allotment() {
-    const [form, setForm] = useState({
-        id_no: '',
-        hotel_name: '',
-        allotment_type: '',
-        supplier_name: '',
-        ref_id: '',
-        local_contact_details: ''
-    })
+  const [form, setForm] = useState({
+    id_no: '',
+    hotel_name: '',
+    allotment_type: '',
+    supplier_name: '',
+    reference_id: '',
+    details: '',
+  })
 
-    const handleChange = ({ target: { name, value } }) => {
-        console.log(form)
-        setForm((p) => ({ ...p, [name]: value }))
-    }
 
-  return(
+  const handleChange = ({ target: { name, value } }) => {
+    // console.log(form)
+    setForm((p) => ({ ...p, [name]: value }))
+  }
+
+  const handleSubmit = () => {
+    _post('api/allotment', form, (resp) => {
+      console.log(resp)
+    }),
+      (err) => {
+        console.log(err)
+      }
+  }
+
+  return (
     <Card className="app_card dashboard_card shadow p-3 m-3">
-        <Row>
-            <Col md={12}>
-                <h5 className="app_title">Allotment</h5>
-            </Col>
-            <Col md={2} style={{display: 'flex',flexDirection: 'row'}}>
-                <InputForm
-                    className="app_input"
-                    label="ID No"
-                    value={form.id_no}
-                    onChange={handleChange}
-                    name="account_number"
-                    type= 'number'
-                />
-            </Col>
-            <Col md={4}>
-                <label className="Label mt-2">Hotel Name</label>
-                <select
-                    id="exampleSelect"
-                    className="app_input"
-                    value={form.hotel_name}
-                    onChange={handleChange}
-                    name="hotel_name"
-                    type="select"
-                >
-                    <option>Select </option>
-                </select>
-            </Col>
-            <Col md={2}>
-                <label className="Label mt-2">Allotment Type</label>
-                <select
-                    id="exampleSelect"
-                    className="app_input"
-                    value={form.allotment_type}
-                    name="allotment_type"
-                    type="select"
-                    onChange={handleChange}
-                >
-                    <option>Select </option>
-                </select>
-            </Col>
-            <Col md={4}>
-                <label className="Label mt-2">Supplier Name</label>
-                <select
-                    id="exampleSelect"
-                    className="app_input"
-                    value={form.supplier_name}
-                    name="supplier_name"
-                    type="select"
-                    onChange={handleChange}
-                >
-                    <option>Select </option>
-                </select>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={2}>
-                <InputForm
-                    className="app_input"
-                    label="Reference ID"
-                    value={form.ref_id}
-                    onChange={handleChange}
-                    name="ref_id"
-                    type= 'number'
-                />
-            </Col>
-            <Col md={3} style={{marginTop: 30}}>
-                <Label >
-                    <Input 
-                        type='checkbox'
-                    /> Show All Pending Transaction
-                </Label>
-            </Col>
-            <Col md={2} style={{marginTop: 30}}>
-                <Label>
-                    <Input 
-                        type='checkbox'
-                    /> Show GI Post
-                </Label>
-            </Col>
-            <Col md={2} style={{marginTop: 30}}>
-                <Label>
-                    <Input 
-                        type='checkbox'
-                    /> Rate Update by Total
-                </Label>
-            </Col>
-        </Row>
-        <Row>
-            <Col md= {4}>
-                <label>Local Contact Details</label>
-                <textarea
-                    className='app_input'
-                    placeholder='While............'
-                    name='branch_address'
-                    // value={form.branch_address}
-                    // onClick={handleChange}
-                />
-            </Col>
-        </Row>
-        <Row>
-            <Col md= {4}>
-                <button
-                    className="app_button p-1"
-                    style={{ width: 200, backgroundColor: 'rgb(12, 134, 103)', height: 40, marginTop: 30}}
-                    // onClick={() => goto('/sign-ip')}
-                    >
-                    Save
-                </button>
-            </Col>
-            <Col md= {4}>
-                <button
-                    className="app_button bg-danger p-1"
-                    style={{ width: 200, backgroundColor: 'rgb(12, 134, 103)', height: 40, marginTop: 30}}
-                    // onClick={() => goto('/sign-ip')}
-                    >
-                    Cancel
-                </button>
-            </Col>
-            <Col md= {4}>
-                <button
-                    className="app_button  p-1"
-                    style={{ width: 200, backgroundColor: 'orange', height: 40, marginTop: 30}}
-                    // onClick={() => goto('/sign-ip')}
-                    >
-                    Add Row
-                </button>
-            </Col>
-        </Row>
-        <AllaotmentTable/>
+      <Row>
+        <Col md={12}>
+          <h5 className="app_title">Allotment</h5>
+        </Col>
+        <Col md={2} style={{ display: 'flex', flexDirection: 'row' }}>
+          <InputForm
+            className="app_input"
+            label="ID No"
+            value={form.id_no}
+            onChange={handleChange}
+            name="id_no"
+            type="number"
+          />
+        </Col>
+        <Col md={4}>
+          <label className="Label mt-2">Hotel Name</label>
+          <select
+            id="exampleSelect"
+            className="app_input"
+            value={form.hotel_name}
+            onChange={handleChange}
+            name="hotel_name"
+            type="select"
+          >
+            <option>HOTEL 2 </option>
+          </select>
+        </Col>
+        <Col md={2}>
+          <label className="Label mt-2">Allotment Type</label>
+          <select
+            id="exampleSelect"
+            className="app_input"
+            value={form.allotment_type}
+            name="allotment_type"
+            type="select"
+            onChange={handleChange}
+          >
+            <option>ALO1 </option>
+            <option>ALO1 </option>
+          </select>
+        </Col>
+        <Col md={4}>
+          <label className="Label mt-2">Supplier Name</label>
+          <select
+            id="exampleSelect"
+            className="app_input"
+            value={form.supplier_name}
+            name="supplier_name"
+            type="select"
+            onChange={handleChange}
+          >
+            <option value='AMD'>AMD </option>
+            <option value='AMv'>AMv </option>
+          </select>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={2}>
+          <InputForm
+            className="app_input"
+            label="Reference ID"
+            value={form.reference_id}
+            onChange={handleChange}
+            name="reference_id"
+            type="number"
+          />
+        </Col>
+        <Col md={3} style={{ marginTop: 30 }}>
+          <Label>
+            <Input type="checkbox" /> Show All Pending Transaction
+          </Label>
+        </Col>
+        <Col md={2} style={{ marginTop: 30 }}>
+          <Label>
+            <Input type="checkbox" /> Show GI Post
+          </Label>
+        </Col>
+        <Col md={2} style={{ marginTop: 30 }}>
+          <Label>
+            <Input type="checkbox" /> Rate Update by Total
+          </Label>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <label>Local Contact Details</label>
+          <textarea
+            className="app_input"
+            placeholder="While............"
+            name="details"
+            value={form.details}
+            onChange={handleChange}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <button
+            className="app_button p-1"
+            style={{
+              width: 200,
+              backgroundColor: 'rgb(12, 134, 103)',
+              height: 40,
+              marginTop: 30,
+            }}
+            onClick={handleSubmit}
+          >
+            Save
+          </button>
+        </Col>
+        <Col md={4}>
+          <button
+            className="app_button bg-danger p-1"
+            style={{
+              width: 200,
+              backgroundColor: 'rgb(12, 134, 103)',
+              height: 40,
+              marginTop: 30,
+            }}
+            // onClick={() => goto('/sign-ip')}
+          >
+            Cancel
+          </button>
+        </Col>
+        <Col md={4}>
+          <button
+            className="app_button  p-1"
+            style={{
+              width: 200,
+              backgroundColor: 'orange',
+              height: 40,
+              marginTop: 30,
+            }}
+            // onClick={() => goto('/sign-ip')}
+          >
+            Add Row
+          </button>
+        </Col>
+      </Row>
+      <AllaotmentTable />
     </Card>
   )
 }
