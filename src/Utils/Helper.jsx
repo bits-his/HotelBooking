d0b93d3992eef2f10dd513736f93ae29d1610cca
nagudu <halifashuaibu12@gmail.com.com>
2023-09-11 14:46:24 +0100

    save

--- src/Utils/Helper.jsx
+++ src/Utils/Helper.jsx
@@ -8,7 +8,7 @@
 import { useLocation } from "react-router";
 // export const server_url = 'https://results-associates.co/server/new-hotel-server'
 // export const server_url = " http://localhost:6678";
-export const server_url = 'https://hotelappnew-e05f332d45bf.herokuapp.com'
+export const server_url = 'https://hotelappnew-e05f332d45bf.herokuapp.com/api' 
 
 export const _post = (url, data, success = (f) => f, error = (f) => f) => {
   const token = localStorage.getItem("@@token");
